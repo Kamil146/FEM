92828058a44edbea71a2983042d580ef4ed87e65
Kamil146 <kamilgomulka@gmail.com>
2021-04-15 15:28:41 +0200

    pierwszy

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
 import numpy as np
 import matplotlib as mat
 
+#Preprocesing
+c=0
+f=0
+x_a=0
+x_b=1
+wezly=np.array([[1,0],
+               [2,1],
+               [3,0.5],
+               [4,0.75]])
+elementy=np.array([[1,1,3],
+                   [2,4,2],
+                   [3,3,4]])
+
+twb_L = 'D'
+twb_P = 'D'
+wwb_L = 0
+wwb_P = 1
+
+
+print("\n",wezly)
+print(elementy)
+
+n=100;
+def generujTabliceGeometrii(p,k,n):
+    tmp = (k-p) / (n-1)
+    matrix = np.array([1,p])
+    print(matrix)
+
+    for i in range(1, n, 1):
+        matrix = np.block([
+            [matrix],
+            [i, i * tmp + p],
+        ])
+    return matrix
+
+
+
+WEZLY = generujTabliceGeometrii(x_a,x_b,n)
+print(WEZLY)
