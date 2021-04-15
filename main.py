2090e2e099e4ed02d0fb10dd1989dbadde54b4fe
Kamil146 <kamilgomulka@gmail.com>
2021-04-15 17:22:37 +0200

    cztery

--- main.py
+++ main.py
@@ -23,13 +23,13 @@ wwb_P = 1
 print("\n",wezly)
 print(elementy)
 
-n=100;
+n=10;
 
 def generujTabliceGeometrii(p,k,n):
     tmp = (k-p) / (n-1)
     matrix = np.array([1,p])
     matrix2 = np.array([1,1,2])
-    print(matrix)
+
 
     for i in range(1, n, 1):
         matrix = np.block([
@@ -48,12 +48,17 @@ def generujTabliceGeometrii(p,k,n):
 
 
 WEZLY,ELEMENTY = generujTabliceGeometrii(x_a,x_b,n)
-print(WEZLY)
-print(ELEMENTY)
 
-y=np.zeros(n)
-x=WEZLY
-plt.plot(x,y)
-plt.show()
 
+def generujGeomtrie(wezly):
+    y=np.zeros(wezly.shape[0])
+    plt.plot(wezly[:,1],y,marker='o')
+    for i in range(0,np.size(y),1):
+        plt.text(x=wezly[i,1],y=y[i]-0.008,s=int(wezly[i,0]),color='red')
+    for i in range(0, np.size(y)-1, 1):
+        plt.text(x=(wezly[i, 1]+wezly[i+1, 1])/2, y=y[i] + 0.005, s=int(i+1),color='blue')
+    plt.show()
+
+
+generujGeomtrie(WEZLY)
 
